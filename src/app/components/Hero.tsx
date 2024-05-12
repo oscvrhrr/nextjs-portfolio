a1dbf5a0a139bdcdc0ce007848a8f033e77c7877
Oscar Herrera <a.oscarherrera@gmail.com>
2024-05-12 16:25:52 -0500

    hero component

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -1,26 +1,42 @@
 import { Nanum_Brush_Script } from "next/font/google"
 import Image from "next/image"
 import Navbar from "./Navbar"
-
+import HeroBackground from "../../../public/assets/hero-background.png"
+import Ufo from "../../../public/assets/svg/ufo.png"
 
 export default function Hero () {
 
     return (
         <section> 
             <Navbar/>
-            <Image
-                src="/assets/hero-image.png"
-                alt="hero image"
-                width={1920}
-                height={400}
-                priority={true}
+            <Image 
+                alt="Outer space"
+                src={HeroBackground}
                 quality={100}
+                className="relative"
             />
+
+            <div className="absolute top-[17rem] left-32 w-[34rem]">
+                <h1 className="text-7xl bg-gradient-to-r from-white to to-zinc-500 text-transparent bg-clip-text">Hey!<br/> My name is Oscar Herrera</h1>
+                <p className="bg-gradient-to-r from-white to to-zinc-500 text-transparent bg-clip-text text-md">
+                    Welcome to my little corner in the web! 
+                    I am passionate aboue learning new tech, and
+                    paving beautiful user experiences through out the 
+                    internet
+                </p>
+            </div>
+
+            <div>
+                <Image 
+                    alt="Oscar"
+                    src={Ufo}
+                    width={350}
+                    height={350}
+                    quality={100}
+                    className="absolute top-[14rem] right-[20rem]"
+                />
+            </div>
         </section>
+    
     )
-
-
-
-
-
 };
--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -3,13 +3,15 @@
 
 
 export default function Navbar () {
+    
+
     return (
-        <nav className="h-20 bg-black flex justify-around items-center">
-            <div>
-                <p className="italic">OSCARDEV</p>
+        <nav className="h-20 bg-black flex justify-between items-center fixed w-full z-10">
+            <div className="ml-28 ">
+                <p className="italic ">OSCARDEV</p>
             </div>
 
-            <ul className="flex w-96 justify-around">
+            <ul className="flex w-96 justify-around mr-28">
                 <li>About</li>
                 <li>Portfolio</li>
                 <li>Contact</li>
