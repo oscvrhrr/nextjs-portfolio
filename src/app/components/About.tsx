dbbc3f71eca9a4b2216abdb8e4d4a4ce5451b2e7
Oscar Herrera <a.oscarherrera@gmail.com>
2024-05-21 08:53:11 -0500

    added about component

---
+++ src/app/components/About.tsx
@@ -0,0 +1,22 @@
+import Image from "next/image";
+import Bitmoji from "../../../public/assets/svg/bitmoji.png";
+
+export default function About() {
+  return (
+    <div className="w-[72.56rem] m-auto my-32">
+      <h2 className="text-6xl">About</h2>
+      <div className="flex items-center h-52 border-y my-4 border-stone-700">
+        <Image alt="" src={Bitmoji} height={200} width={200} />
+        <p className="text-4xl">Click here to learn 5 fun facts about me!</p>
+      </div>
+      <p className="text-3xl">
+        I am a Bay Area native, passionate about learning the mechanics of the
+        objects that surround our daily lives. Curiosity is my main driver
+        leading every decision I make. I fortunatley crossed paths with
+        technology, and instantly fell in love. These Projects serve as a
+        reflection of my learning journey, and each are a stepping stone to my
+        next learning venture.
+      </p>
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import Image from "next/image";
 import Hero from "./components/Hero";
 import Techstack from "./components/Techstack";
+import About from "./components/About";
 
 export default function Home() {
   return (
     <>
       <Hero/>
       <Techstack/>
+      <About/>
     </>
 
   );
