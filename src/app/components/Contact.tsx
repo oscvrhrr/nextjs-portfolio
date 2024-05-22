7ea0cdc557588f54dd332d35af6bd45ec8f7cfe3
Oscar Herrera <a.oscarherrera@gmail.com>
2024-05-22 09:32:23 -0500

    typescript error fix

--- src/app/components/Contact.tsx
+++ src/app/components/Contact.tsx
@@ -1,9 +1,13 @@
 import Image from "next/image"
 import ContactBg from "../../../public/assets/contact-background-img.jpg"
+import { ReactNode } from "react"
 
 
+ type ContactProps = {
+    children: ReactNode
+}
 
-export default function Contact ({children}) {
+export default function Contact ({children}: ContactProps) {
     return (
         <section className="relative h-[55rem]">
             <Image
