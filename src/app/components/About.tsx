5123183d845343564dd5b7fbc14ed7f6ccf7b6a5
Oscar Herrera <a.oscarherrera@gmail.com>
2024-05-24 10:42:06 -0500

    github link animation

--- src/app/components/About.tsx
+++ src/app/components/About.tsx
@@ -12,8 +12,18 @@ export default function About() {
   const paraContainer = useRef<HTMLDivElement>(null);
 
   useGSAP(() => {
-    gsap.to(paraContainer, {
-      
+    gsap.to(paraContainer.current, {
+      scrollTrigger: {
+        trigger: paraContainer.current,
+        start: "start 75%",
+        end: "bottom 150px",
+        markers: true,
+        scrub: 1,
+        toggleActions: "restart pause reverse none"
+      },
+      duration: 2,
+      color: "white",
+      ease: "power1.out"
     })
   });
 
@@ -21,11 +31,11 @@ export default function About() {
   return (
     <div id="GetToKnowMe!" className="w-[72.56rem] m-auto my-32" >
       <h2 className="text-7xl from-[#8E62BC] via-[#4A91E3] to-[#AB2574] bg-gradient-to-r bg-clip-text text-transparent">About me</h2>
-      <div className="flex items-center h-52 border-y my-4 border-stone-700">
+      <div className="flex items-center h-52 border-y my-4 border-stone-800">
         <Image alt="" src={Bitmoji} height={200} width={200} />
         <p className="text-4xl">Click here to learn 5 fun facts about me!</p>
       </div>
-      <p ref={paraContainer} className="text-3xl">
+      <p ref={paraContainer} className="text-3xl text-zinc-700">
         I am a Bay Area native, passionate about learning the mechanics of the
         objects that surround our daily lives. Curiosity is my main driver
         leading every decision I make. I fortunatley crossed paths with
--- src/app/components/Project.tsx
+++ src/app/components/Project.tsx
@@ -1,4 +1,10 @@
+"use client"
 import Image from "next/image"
+import gsap from "gsap"
+import { useRef } from "react"
+import { useGSAP } from "@gsap/react"
+import { ScrollTrigger } from "gsap/all"
+import { reverse } from "dns"
 
 type ProjectProps = {
     cover: string,
@@ -9,6 +15,29 @@ type ProjectProps = {
 
 
 export default function Project ({cover, tech1, tech2, tech3}: ProjectProps) {
+    const animationRef = useRef<GSAPTween | null>(null);
+    const githubContainer = useRef<HTMLDivElement>(null)
+
+    useGSAP(() => {
+       animationRef.current = gsap.to(githubContainer.current, {
+            rotate: 720,
+            duration: .5,
+            paused: true,
+            ease: "power1.in",
+            cursor: "pointer"
+        })
+        
+    });
+
+    const onMouseEnterHandler = () => {
+        animationRef.current?.play()
+    }
+
+    const onMouseLeaveHandler = () => {
+        animationRef.current?.reverse()
+    }
+
+
     return (
         <div className="mb-[14.93rem]">
             <div className="h-[28rem] w-[28rem] bg-gray-800 m-1 mb-10 rounded-3xl shadow-[0_0_35px_0_rgba(0,0,0,0.3)] shadow-[#2563EB] relative">
@@ -29,13 +58,16 @@ export default function Project ({cover, tech1, tech2, tech3}: ProjectProps) {
                     <li className="border rounded border-white inline m-1 py-1 px-2 text-sm">{tech2}</li>
                     <li className="border rounded border-white inline m-1 py-1 px-2 text-sm">{tech3}</li>
                 </div>
-                <Image
-                    alt="Github"
-                    src="../../../../assets/svg/github.svg"
-                    width={30}
-                    height={30}
-                    className="inline"
-                />
+                <div  ref={githubContainer} onMouseEnter={onMouseEnterHandler} onMouseLeave={onMouseLeaveHandler} >
+                    <Image
+                        alt="Github"
+                        src="../../../../assets/svg/github.svg"
+                        width={30}
+                        height={30}
+                        className="inline"
+                       
+                    />
+                </div>
             </div>
             
         </div>
