5f3fc2a4c0a71e649e8256155da1a9c0f486384c
Oscar Herrera <a.oscarherrera@gmail.com>
2024-06-02 15:43:12 -0500

    linked each projects readme. added project descriptions

--- src/app/components/Project.tsx
+++ src/app/components/Project.tsx
@@ -1,75 +1,94 @@
-"use client"
-import Image from "next/image"
-import gsap from "gsap"
-import { useRef } from "react"
-import { useGSAP } from "@gsap/react"
-
-
+"use client";
+import Image from "next/image";
+import gsap from "gsap";
+import { useRef } from "react";
+import { useGSAP } from "@gsap/react";
+import Link from "next/link";
 
 type ProjectProps = {
-    cover: string,
-    tech1: string,
-    tech2: string,
-    tech3: string
-}
-
-
-export default function Project ({cover, tech1, tech2, tech3}: ProjectProps) {
-    const animationRef = useRef<GSAPTween | null>(null);
-    const githubContainer = useRef<HTMLDivElement>(null)
-
-    useGSAP(() => {
-       animationRef.current = gsap.to(githubContainer.current, {
-            rotate: 720,
-            duration: .5,
-            paused: true,
-            ease: "power1.in",
-            cursor: "pointer"
-        })
-        
-    });
-
-    const onMouseEnterHandler = () => {
-        animationRef.current?.play()
-    }
-
-    const onMouseLeaveHandler = () => {
-        animationRef.current?.reverse()
-    }
-
-
-    return (
-        <div className="mb-[14.93rem]">
-            <div className="h-[28rem] w-[28rem] bg-gray-800 m-1 mb-10 rounded-3xl shadow-[0_0_35px_0_rgba(0,0,0,0.3)] shadow-[#2563EB] relative">
-                <Image 
-                    alt="Project"
-                    src={cover}
-                    fill
-                    objectFit="cover"
-                    objectPosition="top"
-                    className="rounded-3xl"
-
-                    
-                />
-            </div>
-            <div className="flex justify-between">
-                <div>
-                    <li className="border rounded border-white inline m-1 py-1 px-2 text-sm">{tech1}</li>
-                    <li className="border rounded border-white inline m-1 py-1 px-2 text-sm">{tech2}</li>
-                    <li className="border rounded border-white inline m-1 py-1 px-2 text-sm">{tech3}</li>
-                </div>
-                <div  ref={githubContainer} onMouseEnter={onMouseEnterHandler} onMouseLeave={onMouseLeaveHandler} >
-                    <Image
-                        alt="Github"
-                        src="../../../../assets/svg/github.svg"
-                        width={30}
-                        height={30}
-                        className="inline"
-                       
-                    />
-                </div>
-            </div>
-            
-        </div>
-    )
+  cover: string;
+  tech1: string;
+  tech2: string;
+  tech3: string;
+  code: string;
+  site: string;
+  text: string;
 };
+
+export default function Project({
+  cover,
+  tech1,
+  tech2,
+  tech3,
+  code,
+  site,
+  text
+}: ProjectProps) {
+  const animationRef = useRef<GSAPTween | null>(null);
+  const githubContainer = useRef<HTMLDivElement>(null);
+
+  useGSAP(() => {
+    animationRef.current = gsap.to(githubContainer.current, {
+      rotate: 720,
+      duration: 0.5,
+      paused: true,
+      ease: "power1.in",
+      cursor: "pointer",
+    });
+  });
+
+  const onMouseEnterHandler = () => {
+    animationRef.current?.play();
+  };
+
+  const onMouseLeaveHandler = () => {
+    animationRef.current?.reverse();
+  };
+
+  return (
+    <div className="mb-[14.93rem]">
+      <div className="h-[28rem] w-[28rem] bg-gray-800 m-1 mb-10 rounded-3xl shadow-[0_0_35px_0_rgba(0,0,0,0.3)] shadow-[#2563EB] relative">
+        <Link href={site} target="_blank">
+            <Image
+            alt="Project"
+            src={cover}
+            fill
+            objectFit="cover"
+            objectPosition="top"
+            className="rounded-3xl"
+            />
+        </Link>
+      </div>
+      <div className="flex justify-between">
+        <div>
+          <li className="border rounded border-white inline m-1 py-1 px-2 text-sm">
+            {tech1}
+          </li>
+          <li className="border rounded border-white inline m-1 py-1 px-2 text-sm">
+            {tech2}
+          </li>
+          <li className="border rounded border-white inline m-1 py-1 px-2 text-sm">
+            {tech3}
+          </li>
+        </div>
+        <div
+          ref={githubContainer}
+          onMouseEnter={onMouseEnterHandler}
+          onMouseLeave={onMouseLeaveHandler}
+        >
+
+          <Link href={code} target="_blank">
+            <Image
+              alt="Github"
+              src="../../../../assets/svg/github.svg"
+              width={30}
+              height={30}
+              className="inline"
+            />
+          </Link>
+        </div>
+      </div>
+      <p className="w-80 text-lg mt-4">{text}</p>
+    </div>
+  );
+}
--- src/app/components/ProjectHub.tsx
+++ src/app/components/ProjectHub.tsx
@@ -5,7 +5,7 @@ const imagePaths = {
   ecommerce: "/assets/project-image-1.jpg",
   musicapp: "/assets/project-image-2.jpg",
   photosnap: "/assets/project-image-3.jpg",
-  dine: "/assets/project-image-4.jpg"
+  dine: "/assets/project-image-4.jpg",
 };
 
 export default function ProjectHub() {
@@ -18,38 +18,45 @@ export default function ProjectHub() {
         Portfolio
       </h2>
       <div className="w-[72.56rem] flex flex-wrap m-auto justify-between">
-        <Link href={"https://ecommerce-react-audiophile.netlify.app/"} target="_blank">
-          <Project
-            cover={imagePaths.ecommerce}
-            tech1="React"
-            tech2="SCSS"
-            tech3="Vite"
-          />
-        </Link>
-        <Link href={"https://music-app-react-five.vercel.app/"} target="_blank">
-            <Project
-            cover={imagePaths.musicapp}
-            tech1="TypeScript"
-            tech2="Next.js"
-            tech3="Tailwind"
-            />
-        </Link>
-        <Link href={"https://oscvrhrr.github.io/photosnap-multi-pg-website/"} target="_blank">
-            <Project
-            cover={imagePaths.photosnap}
-            tech1="HTML"
-            tech2="CSS"
-            tech3="JavaScript"
-            />
-        </Link>
-        <Link href={"https://oscvrhrr.github.io/dine-restaurant/"} target="_blank">
-            <Project
-            cover={imagePaths.dine}
-            tech1="HTML"
-            tech2="CSS"
-            tech3="JavaScript"
-            />
-        </Link>
+        <Project
+          cover={imagePaths.ecommerce}
+          tech1="React"
+          tech2="SCSS"
+          tech3="Vite"
+          site="https://ecommerce-react-audiophile.netlify.app/"
+          code="https://github.com/oscvrhrr/ecommerce-store"
+          text="Audiophile an e-commerce store. A fully responsive React App, on mobile, iPad, and Desktop."
+        />
+
+        <Project
+          cover={imagePaths.musicapp}
+          tech1="TypeScript"
+          tech2="Next.js"
+          tech3="Tailwind"
+          site="https://music-app-react-five.vercel.app/"
+          code="https://github.com/oscvrhrr/music-app-react"
+          text="Musica an app thats connected to spotify's RESTapi. Relax and enjoy the tunes."
+        />
+
+        <Project
+          cover={imagePaths.photosnap}
+          tech1="HTML"
+          tech2="CSS"
+          tech3="JavaScript"
+          site="https://oscvrhrr.github.io/photosnap-multi-pg-website/"
+          code="https://github.com/oscvrhrr/photosnap-multi-pg-website"
+          text="PhotoSnap a multi-page marketing site. Fully responsive on mobile, iPad, and desktop."
+        />
+
+        <Project
+          cover={imagePaths.dine}
+          tech1="HTML"
+          tech2="CSS"
+          tech3="JavaScript"
+          site="https://oscvrhrr.github.io/dine-restaurant/"
+          code="https://github.com/oscvrhrr/dine-restaurant"
+          text="A restaurant website that allows users to book reservations. Fully responsive on mobile, iPad, and desktop."
+        />
       </div>
     </>
   );
