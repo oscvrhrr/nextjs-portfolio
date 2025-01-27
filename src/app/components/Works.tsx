2f5e99a0a79bd1a9cac31d02abbf6429277789b7
Oscar Herrera <a.oscarherrera@gmail.com>
2025-01-27 08:41:45 -0600

    added new project

--- src/app/components/Works.tsx
+++ src/app/components/Works.tsx
@@ -10,7 +10,7 @@ const Works = () => {
       <Flex direction="column" gap="5">
         <Heading size="7">Projects</Heading>
         <Text>Some of my latest projects</Text>
-        <Grid columns="2" gap="6" gapY="8" rows="repeat(2, 240px)">
+        <Grid columns="2" gap="6" gapY="8" rows="repeat(4, 240px)">
           <AspectRatio ratio={16 / 8} >
             <img
               src="https://fzyxhpuljtyplklakuoy.supabase.co/storage/v1/object/sign/misc/mydrive.gif?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJtaXNjL215ZHJpdmUuZ2lmIiwiaWF0IjoxNzM0NzIxNTMxLCJleHAiOjE3NjYyNTc1MzF9.5I59XBDsaJlbBevBsl499wdIzf2VPTyTsoV1s_sYwmI&t=2024-12-20T19%3A05%3A31.674Z"
@@ -40,22 +40,6 @@ const Works = () => {
                 MyDrive is a File storage system its a stripped down version of google drive. Feel free use the demo account yourself
             </Text>
           </Flex>
-          <AspectRatio ratio={16 / 8} >
-         
-            <img
-              src="https://fzyxhpuljtyplklakuoy.supabase.co/storage/v1/object/sign/misc/blogapi-ezgif.com-optimize.gif?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJtaXNjL2Jsb2dhcGktZXpnaWYuY29tLW9wdGltaXplLmdpZiIsImlhdCI6MTczNzU2NDA5MSwiZXhwIjoxNzY5MTAwMDkxfQ.sbuV1k33n6evFBnJ52lFQb853G_4-AimjFsYy60egL0&t=2025-01-22T16%3A41%3A31.937Z"
-              alt="A house in a forest"
-              style={{
-                objectFit: "cover",
-                width: "100%",
-                height: "100%",
-                borderRadius: "var(--radius-2)",
-                boxShadow: "var(--shadow-1)"
-              }}
-              
-              />
-              
-          </AspectRatio>
           <Flex direction="column">
             <Flex align="center">
               <Text size="6">
@@ -72,8 +56,52 @@ const Works = () => {
                 This is a blog app, I integrated tinyMCE text editor to enable rich content creation. I loved working prismjs, which allows code snippets to be highlighted
             </Text>
           </Flex>
-         
-          
+          <AspectRatio ratio={16 / 8} >
+            <img
+              src="https://fzyxhpuljtyplklakuoy.supabase.co/storage/v1/object/sign/misc/blogapi-ezgif.com-optimize.gif?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJtaXNjL2Jsb2dhcGktZXpnaWYuY29tLW9wdGltaXplLmdpZiIsImlhdCI6MTczNzU2NDA5MSwiZXhwIjoxNzY5MTAwMDkxfQ.sbuV1k33n6evFBnJ52lFQb853G_4-AimjFsYy60egL0&t=2025-01-22T16%3A41%3A31.937Z"
+              alt="A house in a forest"
+              style={{
+                objectFit: "cover",
+                width: "100%",
+                height: "100%",
+                borderRadius: "var(--radius-2)",
+                boxShadow: "var(--shadow-1)"
+              }}
+              
+              />
+          </AspectRatio>
+          <AspectRatio ratio={16 / 8} >
+            <img
+              src="https://fzyxhpuljtyplklakuoy.supabase.co/storage/v1/object/sign/misc/audiophile-desktop.jpg?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJtaXNjL2F1ZGlvcGhpbGUtZGVza3RvcC5qcGciLCJpYXQiOjE3Mzc5ODgyNzgsImV4cCI6MTc2OTUyNDI3OH0.7nQLiTCZ2sPH0aLxyYjjVGN4mQPkVlgbn6cmTaAcQ4M"
+              alt="A house in a forest"
+              style={{
+                objectFit: "cover",
+                width: "100%",
+                height: "100%",
+                borderRadius: "var(--radius-2)",
+                boxShadow: "var(--shadow-1)"
+              }}
+              
+              />
+          </AspectRatio>
+          <Flex direction="column">
+            <Flex align="center">
+              <Text size="6">
+                  Audiophile
+              </Text>
+              <Link href="https://ecommerce-react-audiophile.netlify.app/" target="_blank">
+                <ExternalLinkIcon className="ml-2"/>
+              </Link>
+              <Link href="https://github.com/oscvrhrr/ecommerce-store" target="_blank">
+                <GitHubLogoIcon className="ml-2"/>
+              </Link>
+            </Flex>
+            <Text size="3">
+              Audiophile is a <Link className="underline" href="https://www.frontendmentor.io/">Frontend Mentor</Link> challenge, its 
+              a platfrom that provides Figma designs for frontend developer to practice real life work flows. This is built with React and 
+              is a fully responsive for various screen sizes
+            </Text>
+          </Flex>
         </Grid>
       </Flex>
     </Container>
