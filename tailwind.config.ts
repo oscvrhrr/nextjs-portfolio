e6acb4df359598e3fe6cf09b4cb0d3301b761c0c
Oscar Herrera <a.oscarherrera@gmail.com>
2025-05-15 09:57:01 -0500

    feat: added skeletons images

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,50 +1,95 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
+    darkMode: ["class"],
+    content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        'light-accent': {
-          100: '#FCFDFC',
-          200: '#F8FAF8',
-          300: '#EFF1EF',
-          400: '#E7E9E6',
-          500: '#DFE2DF',
-          600: '#D7DAD7',
-          700: '#CCCFCC',
-          800: '#B9BCB8',
-          900: '#1D211C',
-          1000: '#323731',
-          1100: '#60655F',
-          1200: '#1D211C',
-        },
-        'light-gray': {
-          100: '#FCFCFD',
-          200: '#F9F9FB',
-          300: '#EFF0F3',
-          400: '#E7E8EC',
-          500: '#E0E1E6',
-          600: '#D7DAD7',
-          700: '#CCCFCC',
-          800: '#B9BBC6',
-          900: '#8B8D98',
-          1000: '#80828D',
-          1100: '#62636C',
-          1200: '#1E1F24',
-        }
-      },
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
+  	extend: {
+  		colors: {
+  			'light-accent': {
+  				'100': '#FCFDFC',
+  				'200': '#F8FAF8',
+  				'300': '#EFF1EF',
+  				'400': '#E7E9E6',
+  				'500': '#DFE2DF',
+  				'600': '#D7DAD7',
+  				'700': '#CCCFCC',
+  				'800': '#B9BCB8',
+  				'900': '#1D211C',
+  				'1000': '#323731',
+  				'1100': '#60655F',
+  				'1200': '#1D211C'
+  			},
+  			'light-gray': {
+  				'100': '#FCFCFD',
+  				'200': '#F9F9FB',
+  				'300': '#EFF0F3',
+  				'400': '#E7E8EC',
+  				'500': '#E0E1E6',
+  				'600': '#D7DAD7',
+  				'700': '#CCCFCC',
+  				'800': '#B9BBC6',
+  				'900': '#8B8D98',
+  				'1000': '#80828D',
+  				'1100': '#62636C',
+  				'1200': '#1E1F24'
+  			},
+  			background: 'hsl(var(--background))',
+  			foreground: 'hsl(var(--foreground))',
+  			card: {
+  				DEFAULT: 'hsl(var(--card))',
+  				foreground: 'hsl(var(--card-foreground))'
+  			},
+  			popover: {
+  				DEFAULT: 'hsl(var(--popover))',
+  				foreground: 'hsl(var(--popover-foreground))'
+  			},
+  			primary: {
+  				DEFAULT: 'hsl(var(--primary))',
+  				foreground: 'hsl(var(--primary-foreground))'
+  			},
+  			secondary: {
+  				DEFAULT: 'hsl(var(--secondary))',
+  				foreground: 'hsl(var(--secondary-foreground))'
+  			},
+  			muted: {
+  				DEFAULT: 'hsl(var(--muted))',
+  				foreground: 'hsl(var(--muted-foreground))'
+  			},
+  			accent: {
+  				DEFAULT: 'hsl(var(--accent))',
+  				foreground: 'hsl(var(--accent-foreground))'
+  			},
+  			destructive: {
+  				DEFAULT: 'hsl(var(--destructive))',
+  				foreground: 'hsl(var(--destructive-foreground))'
+  			},
+  			border: 'hsl(var(--border))',
+  			input: 'hsl(var(--input))',
+  			ring: 'hsl(var(--ring))',
+  			chart: {
+  				'1': 'hsl(var(--chart-1))',
+  				'2': 'hsl(var(--chart-2))',
+  				'3': 'hsl(var(--chart-3))',
+  				'4': 'hsl(var(--chart-4))',
+  				'5': 'hsl(var(--chart-5))'
+  			}
+  		},
+  		backgroundImage: {
+  			'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+  			'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))'
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		}
+  	}
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 };
 export default config;
